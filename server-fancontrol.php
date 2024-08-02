fa2e98dba9b3d949d4d8776d166f9c810994fae4
Jmoati <jacques@moati.net>
2024-08-02 23:59:58 +0200

    Add a devContainer to develop easier Use ipmi for query temperature to be able to run on another computer Adjust some param for my config

--- server-fancontrol.php
+++ server-fancontrol.php
@@ -2,66 +2,56 @@
 
 // Based on following solution: https://askubuntu.com/a/1312802/1067698
 // This code is for the following server configuration:
-// Dell R610, 24 x Intel(R) Xeon(R) CPU X5690 @ 3.47GHz (2 Sockets)
+// Dell PowerEdge R730xd, 36 x Intel(R) Xeon(R) CPU E5-2699 v3 @ 2.30GHz (2 Sockets)
 
 // This code must be modified according TO YOUR OWN server configuration!
-
-// Make sure you have installed "ipmitools" and you can see the output of command "sensors"
-
 // USE AT YOUR OWN RISK
-// E-Mail: fred@coldrocksoftware.com
 
-const MIN_FAN = 5;
+const MIN_FAN = 10;
 const MAX_FAN = 100;
-
-const MIN_TEMP = 45; # adjust based on sensors output
-const MAX_TEMP = 75; # adjust based on sensors output
-
+const MIN_TEMP = 50;
+const MAX_TEMP = 80;
 const TEMP_POW = 3; # decrease for cooler server, increase for quiter
 
-system('clear');
+if (!file_exists('.env')) {
+    throw new Exception('Please, create a .env file with IPMI_IP, IPMI_LOGIN and IPMI_PASS');
+}
 
-$sensorsPath = trim(shell_exec('which sensors'));
-$sensorData = trim(shell_exec($sensorsPath . ' -j'));
-$decodedData = json_decode(trim($sensorData), true);
+$env = parse_ini_file('.env');
 
-$coreTemperatures = [];
+$ipmitool = sprintf(
+    'ipmitool -I lanplus -H %s  -U %s -P %s',
+    $env['IPMI_IP'],
+    $env['IPMI_LOGIN'],
+    $env['IPMI_PASS']
+);
 
-foreach ($decodedData as $core => $coreData) {
-    foreach ($coreData as $coreItem => $coreItemData) {
-        if (stristr($coreItem, 'Adapter')) {
-            continue;
-        }
+while(1) {
+    $temperaturesRaw = explode(PHP_EOL, trim(shell_exec(sprintf('%s sensor reading "Inlet Temp" "Temp" "Exhaust Temp" -c', $ipmitool))));
+    $temperatures = [];
 
-        foreach ($coreItemData as $index => $value) {
-            if (!stristr($index, '_input')) {
-                continue;
-            }
-
-            $coreTemperatures[$core][] = $value;
-        }
+    foreach ($temperaturesRaw as $temperatureRaw) {
+        list($key, $value) = explode(',', $temperatureRaw);
+        $temperatures[$key] = (int)$value;
     }
+
+    if (!array_key_exists('Temp', $temperatures)) {
+        sleep(1);
+        continue;
+    }
+
+    $x = min(1, max(0, ($temperatures['Temp'] - MIN_TEMP) / (MAX_TEMP - MIN_TEMP)));
+    $fanLevel = (int)min(MAX_FAN, max(MIN_FAN, pow($x, TEMP_POW)*(MAX_FAN-MIN_FAN) + MIN_FAN));
+    $fanLevelHex = '0x' .dechex($fanLevel);
+
+    shell_exec(sprintf('%s raw 0x30 0x30 0x01 0x00', $ipmitool));
+    shell_exec(sprintf('%s raw 0x30 0x30 0x02 0xff %s', $ipmitool, $fanLevelHex));
+
+    system('clear');
+    echo sprintf('Temperature system : %d°C%s', $temperatures['Temp'],PHP_EOL);
+    echo sprintf('Temperature in : %d°C%s',$temperatures['Inlet Temp'],PHP_EOL);
+    echo sprintf('Temperature out temperature: %d°C%s', $temperatures['Exhaust Temp'], PHP_EOL);
+    echo sprintf('Fan level to set: %d%%%s', $fanLevel, PHP_EOL);
+
+    sleep(10);
 }
-
-$averages = [];
-foreach ($coreTemperatures as $module => $temperatures) {
-    $a = array_filter($temperatures);
-    $average = array_sum($a)/count($a);
-    $averages[$module] = ceil($average);
-}
-
-$finalRawAverages = array_values($averages);
-$finalAverage = ceil(array_sum($finalRawAverages)/count($finalRawAverages));
-
-$x = min(1, max(0, ($finalAverage - MIN_TEMP) / (MAX_TEMP - MIN_TEMP)));
-$fanLevel = (int)min(MAX_FAN, max(MIN_FAN, pow($x, TEMP_POW)*(MAX_FAN-MIN_FAN) + MIN_FAN));
-$fanLevelHex = '0x' .dechex($fanLevel);
-
-echo 'System average temperature: ' . $finalAverage . PHP_EOL;
-echo 'Fan level to set: ' . $fanLevel . PHP_EOL;
-echo 'Fan level hex: ' . $fanLevelHex . PHP_EOL;
-
-shell_exec("ipmitool raw 0x30 0x30 0x01 0x00");
-shell_exec("ipmitool raw 0x30 0x30 0x02 0xff {$fanLevelHex}");
-
-echo "Fan speed was set to the new levels" . PHP_EOL;
